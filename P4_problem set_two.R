9125c3e8e9194c7db1c507b2f77a877a7f298617
Akshith <akshith.rk@gmail.com>
2016-11-07 21:44:46 -0500

    eda & correlations

--- P4_problem set_two.R
+++ P4_problem set_two.R
@@ -35,6 +35,9 @@ colnames(diamonds)
 ??by
 by(diamonds$clarity, nrow(diamonds$carat), simplify = T)
 
+
+# defining variables
+
 carat <- diamonds$carat
 cut <- diamonds$cut
 color <- diamonds$color
@@ -46,3 +49,23 @@ x <- diamonds$x
 y <- diamonds$y
 z <- diamonds$z
 
+# price vs x
+
+p1 <- ggplot(aes(x = price, y = x), data = diamonds) +
+  geom_point()
+
+p1
+
+# findings
+
+cor.test(price, x, alternative = 'two.sided',
+         method = 'pearson', conf.level = 0.95 )
+
+# correlations
+cor.test(price, y, alternative = 'two.sided',
+         method = 'pearson', conf.level = 0.95 )
+
+cor.test(price, z, alternative = 'two.sided',
+         method = 'pearson', conf.level = 0.95 )
+
+#price vs depth
